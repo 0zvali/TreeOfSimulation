216e98ede99829748b215bd0f77b946a1f85132d
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2023-11-22 12:22:45 -0500

    Update a.js

--- js/layers/a.js
+++ js/layers/a.js
@@ -212,10 +212,10 @@ addLayer("a", {
             tooltip(){return hasAchievement("a", 46)?`<h4 style='color: purple; text-shadow: darkred 1.75px 1.75px 10px;'> Uh oh...You've collapsed the timeline!</h4>`:"Get the 6th Room Upgrade"},
             },
         66: {
-            name(){return hasAchievement("a", 56)?`<h4 style='color: red; text-shadow: blue 1.75px 1.75px 10px;'>I Knew you were going to do that.</h4>`: "I knew you were going to do that."},
+            name(){return hasAchievement("a", 66)?`<h4 style='color: red; text-shadow: blue 1.75px 1.75px 10px;'>I Knew you were going to do that.</h4>`: "I knew you were going to do that."},
             done() { return player.devSpeed>=1.01 },
-            tooltip(){return hasAchievement("a", 56)?`<h4 style='color: red; text-shadow: blue 1.75px 1.75px 10px;'> You're going to play fairly!<br>So your Infects & Crystal gain is /1e300 until you've set your devSpeed back or under 1.<br> You've found a secret achievement that will never go away!</h4>`:"  "},
-            unlocked(){return player.devSpeed>=1.01 || hasAchievement("a", 56)},
+            tooltip(){return hasAchievement("a", 66)?`<h4 style='color: red; text-shadow: blue 1.75px 1.75px 10px;'> You're going to play fairly!<br>So your Infects & Crystal gain is /1e300 until you've set your devSpeed back or under 1.<br> You've found a secret achievement that will never go away!</h4>`:"  "},
+            unlocked(){return player.devSpeed>=1.01 || hasAchievement("a", 66)},
             },
         51: {
             name: "Building Floors",
