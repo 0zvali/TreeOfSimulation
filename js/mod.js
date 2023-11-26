4cc5c7eaee54861461b40bdd5aacf6ee4e3e66ec
Acythe3 <marcusjacksondred@gmail.com>
2023-11-26 05:28:00 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -346,7 +346,7 @@ function canGenPoints(){
 // Calculate points/sec!
 function getPointGen() {
 	if(!canGenPoints())
-		return new Decimal(0)
+		return new Decimal(1)
 
 	let gain = new Decimal(1)
 	// C Upgrades
