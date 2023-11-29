7f9e01553ed60b7854cafe1b092d7a12d3edc7a3
Acythe3 <marcusjacksondred@gmail.com>
2023-11-28 22:07:20 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -452,7 +452,7 @@ function addedPlayerData() { return {
 var displayThings = [
 	"<a>Endgame: 1 Soul</a>",
 	function() {
-		return format(intervalArray)
+		return format(ticker)
 	  },
 
 	function() {
