96ec944ed61c584e5146d32e17a20086211679a9
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2023-11-30 14:33:24 -0500

    some refix

--- js/layers/timeline_SL.js
+++ js/layers/timeline_SL.js
@@ -43,7 +43,7 @@ addLayer("SL", {
     },
     effectDescription() {
         dis = "which is boosting floor gain by "+format(tmp.SL.effect)+"x"
-        if (hasUpgrade('SL', 21)) dis = "which is boosting floor gain by "+format(tmp.SL.effect)+`x<br>` + "and dividing explosive & floor requirement by /"+format(player.SL.points.max(1).add(1).pow(0.35).div(3))
+        if (hasUpgrade('SL', 21)) dis = "which is boosting floor gain by "+format(tmp.SL.effect)+`x<br>` + "and dividing explosive & floor requirement by /"+format((player.SL.points.max(1).add(1).pow(0.35).div(3)).max(1))
         return dis
     },
     effectBase() {
