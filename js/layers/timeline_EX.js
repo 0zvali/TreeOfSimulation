df570c19381c82b61353e4e9c76bc16c826ee05b
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2023-12-01 19:05:55 -0500

    Update timeline_EX.js

--- js/layers/timeline_EX.js
+++ js/layers/timeline_EX.js
@@ -47,9 +47,6 @@ addLayer("EX", {
     },
     passiveGeneration() {
         let value1 = new Decimal(0);
-        if (challengeCompletions("CT", 22) = 2) value1 = value1.add(0.25);
-        if (challengeCompletions('CT', 22) = 3) value1 = value1.add(0.25);
-        if (challengeCompletions("CT", 22) = 4) value1 = value1.add(0.4);
         return value1;
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)
