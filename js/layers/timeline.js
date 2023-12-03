1722eeb1e6a4edf279b53c2f6920ea29bc9b28c1
Acythe3 <marcusjacksondred@gmail.com>
2023-12-03 15:11:21 -0500

    Update timeline.js

--- js/layers/timeline.js
+++ js/layers/timeline.js
@@ -122,8 +122,6 @@ challenges: {
             canComplete: function() {return player.SL.points.gte(1e25)},
             goalDescription: "1e25 Souls",
             rewardDescription() { return "Souls boosts itself (" + (format(player.SL.points.add(1).pow(0.06)) + "x)") + `<br> Keep Floors & Explosive Upgrades.`},
-            onEnter(){player.SL.upgrades.push("11", "12", "13", "14", "15", "21") = true},
-            onExit(){player.SL.upgrades.push("11", "12", "13", "14", "15", "21") = true},
             unlocked(){
                 return hasUpgrade('SL', 21) || inChallenge('CT', 22) || hasChallenge('CT', 22)
             },
