41f3b9d29f3578e19e84e3d9b1668f6f7d025b3c
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2024-01-07 08:56:49 -0500

    Update meta_C.js

--- js/layers/meta_C.js
+++ js/layers/meta_C.js
@@ -1,5 +1,5 @@
 addLayer("mC", {
-    name: "Meta Crystals", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "Meta-Crystals", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "mC", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 4, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -15,7 +15,7 @@ addLayer("mC", {
         return requirement
         
     }, // Can be a function that takes requirement increases into account
-    resource: "Meta Crystals", // Name of prestige currency
+    resource: "Meta-Crystals", // Name of prestige currency
     baseResource: "infects",
     baseAmount() {return player.points}, 
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
