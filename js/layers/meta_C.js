410eefe6b1b2df0f61b9d14c05db9afa41bac171
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2024-01-07 08:30:30 -0500

    Update meta_C.js

--- js/layers/meta_C.js
+++ js/layers/meta_C.js
@@ -22,6 +22,7 @@ addLayer("mC", {
     exponent(){ 
               let expo = 0.39
               if (hasUpgrade('mC', 15)) expo = 0.43 
+ },
               return expo // Prestige currency exponent (can be changed/adjusted)
     gainMult() { // Calculate the multiplier for main currency from bonuses
         let mult = new Decimal(1)
