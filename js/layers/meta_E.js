f45a2e3cd11a07b72bca18e6352ec31c0efa0898
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2024-01-12 14:28:40 -0500

    minor refix i guess shrug..

--- js/layers/meta_E.js
+++ js/layers/meta_E.js
@@ -137,7 +137,7 @@ milestones: {
                 return new Decimal(1.5e8).mul(Decimal.pow(1.125, x)).mul(Decimal.pow(x , Decimal.pow(exp2 , x))).floor()
             },
             display() {
-                return "Cost: " + format(tmp[this.layer].buyables[this.id].cost) + " Meta-Experiments" + "<br>Bought: " + getBuyableAmount(this.layer, this.id) + "<br>Effect: 'Experimental Regime I' & 'Experimental Regime II' cost is /" + format(buyableEffect(this.layer, this.id)) + "cheaper; and increase Meta-Experiment gain by x" + format(buyableEffect(this.layer, this.id).div(1.5)) 
+                return "Cost: " + format(tmp[this.layer].buyables[this.id].cost) + " Meta-Experiments" + "<br>Bought: " + getBuyableAmount(this.layer, this.id) + "<br>Effect: 'Experimental Regime I' & 'Experimental Regime II' cost is /" + format(buyableEffect(this.layer, this.id)) + " cheaper; and increase Meta-Experiment gain by x" + format(buyableEffect(this.layer, this.id).div(1.5)) 
             },
             canAfford() {
                 return player[this.layer].points.gte(this.cost())
