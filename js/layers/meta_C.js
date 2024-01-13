eeaf7d9045bde5e602ab175dc99a73ba814b9ebc
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2024-01-13 08:08:06 -0500

    Update meta_C.js

--- js/layers/meta_C.js
+++ js/layers/meta_C.js
@@ -68,7 +68,6 @@ addLayer("mC", {
         if (layers[resettingLayer].row > this.row) layerDataReset(this.layer)
         if (hasMilestone('mE', 12)) player.mC.upgrades.push("11", "12", "13", "14", "15" ,"16", "21", "22", "23", "24", "25", "26")
         if (hasMilestone('mE', 12)) player.mC.milestones.push("11", "12")
-    },
        if (hasMilestone('mE', 16)) player.mC.milestones.push("13")
     },
     passiveGeneration() {
