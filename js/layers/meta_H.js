50ece579c18cb50ce71f3abe9c390a1c9b544d1d
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2024-01-24 13:16:27 -0500

    ok

--- js/layers/meta_H.js
+++ js/layers/meta_H.js
@@ -48,7 +48,7 @@ addLayer("mH", {
     },
     canBuyMax(){
         let prestige = false
-        if (hasMilestone('mH', 14)) prestige = true
+        if (hasMilestone('mH', 12)) prestige = true
         return prestige
     },
     resetsNothing(){
@@ -65,7 +65,7 @@ milestones: {
         },
         12: {
             requirementDescription: "3 Meta-Humans",
-            effectDescription: `Keep the current Meta-Experiment Milestones & Upgrades`,
+            effectDescription: `Keep the current Meta-Experiment Milestones & Upgrades; while having that, you can buy the max amount of Meta-Humans possible.`,
             done() { return player.mH.points.gte(3) },
         },
         13: {
@@ -76,7 +76,7 @@ milestones: {
         },
         14: {
             requirementDescription: "35 Meta-Humans",
-            effectDescription: `Automate Meta-Humans, and if you want Buy Max amount of Meta-Humans. With that, unlock a Meta-Human Buyable`,
+            effectDescription: `Automate Meta-Humans. With that, unlock a Meta-Human Buyable`,
             done() { return player.mH.points.gte(35) },
             unlocked(){ return hasMilestone('mH', 13) }, 
         },
