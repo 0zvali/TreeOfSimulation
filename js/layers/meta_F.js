8154a512cdc553dd3c474304adf6437be09b6b4e
E205-Idle <127055307+E205-Idle@users.noreply.github.com>
2024-01-25 09:08:42 -0500

    u

--- js/layers/meta_F.js
+++ js/layers/meta_F.js
@@ -166,7 +166,7 @@ buyables: {
                 return new Decimal(costdef).mul(Decimal.pow(exp1, x)).mul(Decimal.pow(x , Decimal.pow(exp2 , x))).floor()
             },
             display() {
-                return "Cost: " + format(tmp[this.layer].buyables[this.id].cost) + " Meta-Fusions" + "<br>Bought: " + getBuyableAmount(this.layer, this.id)+"/"+ format(purchaseLimit(this.layer,this.id)) + "<br>Effect: Boost Meta-Experiment gain by x" + format(buyableEffect(this.layer, this.id))
+                return "Cost: " + format(tmp[this.layer].buyables[this.id].cost) + " Meta-Fusions" + "<br>Bought: " + getBuyableAmount(this.layer, this.id)+"/"+ format(this.purchaseLimit) + "<br>Effect: Boost Meta-Experiment gain by x" + format(buyableEffect(this.layer, this.id))
             },
             canAfford() {
                 return player[this.layer].points.gte(this.cost())
